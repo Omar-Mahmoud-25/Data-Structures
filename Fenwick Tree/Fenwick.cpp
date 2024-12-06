016d62bb8848439b2c737143a9713776f41aca31
Omar-Mahmoud-25 <omar.mahmoud25102004@gmail.com>
2024-12-06 10:28:47 +0200

    added fenwick tree

---
+++ Fenwick Tree/Fenwick.cpp
@@ -0,0 +1,32 @@
+#include "Fenwick.hpp"
+#define ll long long
+using namespace std;
+
+FenwickTree::FenwickTree(vector<int>& v){
+    tree.resize(v.size() + 1, 0);
+    for (int i = 0; i < v.size(); i++)
+        update(i+1, v[i]);
+}
+
+ll FenwickTree::query(int postion){
+    ll sum = 0;
+    while (postion){
+        sum += tree[postion];
+        postion -= (postion & -postion);
+    }
+}
+
+ll FenwickTree::query(int x,int y){
+    if (x > y)
+        swap(x,y);
+    return query(y) - query(x-1);
+}
+
+void FenwickTree::update(int postion, int value){
+    ll diff = value - query(postion,postion++);
+    while (postion < tree.size()){
+        tree[postion] += diff;
+        postion += postion & -postion;
+    }
+}
+
---
+++ Fenwick Tree/Fenwick.hpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+class FenwickTree{
+private:
+    vector<ll> tree;
+    ll query(int postion);
+public:
+    FenwickTree(vector<int>& v);
+    void update(int postion, int value);
+    ll query(int x, int y);
+};
